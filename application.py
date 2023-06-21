ef53c91be5e2f83456dea38a51e21a29fb241978
Salomão Gonçalves Calheiros <74385370+manscalh@users.noreply.github.com>
2023-06-21 09:25:03 -0400

    Update application.py

--- application.py
+++ application.py
@@ -759,4 +759,4 @@ def func(n_clicks,selection,df_origem):
         return dcc.send_file("relatorio.pdf")
 
 if __name__ == '__main__':
-    application.run(debug=True,host='0.0.0.0')
+    application.run_server(debug=False)
